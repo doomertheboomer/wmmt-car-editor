5fcac10ed2501ab31bb76a9974ea8753b7726794
Damon Murdoch <Damon.Murdoch@qed.qld.gov.au>
2021-12-13 15:37:55 +1000

    Adding todo list to locations file

--- data/locations.js
+++ data/locations.js
@@ -1,20 +1,36 @@
 const LOCATIONS = {
+
+    // CAR & COLOUR SECTION
+    
     'cars': [3, 4],
+    'colour': [], // TODO
+    'rims-colour': [4, 4], // TODO
+
+    // AERO SECTION
+    
     'rims': [4, 0],
-    'rims-colour': [4, 4],
     'aero': [4, 8],
     'hood': [4, 12],
     'wing': [5, 8],
     'mirror': [5, 12],
     'neon': [6, 0],
     'trunk': [6, 4],
-    'plate-frame': [6, 8],
-    'plate-frame-colour': [6, 12],
-    'plate-number1': [7, 0],
-    'plate-number2': [7, 1],
-    'power': [7, 4],
-    'handling': [8, 0],
-    'rank': [8, 4],
-    'sticker-type': [9, 4],
-    'sticker-id': [9, 8],
+
+    // PLATE SECTION
+
+    'plate-frame': [6, 8], // TODO
+    'plate-frame-colour': [6, 12], // TODO
+    'plate-number1': [7, 0], // TODO
+    'plate-number2': [7, 1], // TODO
+
+    // OTHER SECTION
+
+    'power': [7, 4], // TODO
+    'handling': [8, 0], // TODO
+    'rank': [8, 4], // TODO
+
+    // STICKER SECTION
+
+    'sticker-type': [9, 4], // TODO
+    'sticker-id': [9, 8], // TODO
 }
